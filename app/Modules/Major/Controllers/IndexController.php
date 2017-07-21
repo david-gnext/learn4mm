3e483c0b8555a8a289a4c4d0a28d9b19af07d8a2
david <david.gnext@gmail.com>
2017-07-21 08:51:24 +0630

    Merge origin/master into 5154
    Conflicts:
    	public/apps/schedule/css/group.css
    	public/apps/schedule/css/group.css.map

---
+++ app/Modules/Major/Controllers/IndexController.php
@@ -0,0 +1,33 @@
+
+<?php
+
+namespace App\Modules\Major\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class IndexController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+    public function index() {
+        echo "Index Multi";
+    }
+}
