b352f1ea48882736435476496327845d49c39c28
david <david.gnext@gmail.com>
2017-12-01 13:41:52 +0630

    middleware

--- app/Modules/User/routes.php
+++ app/Modules/User/routes.php
@@ -1,3 +1,5 @@
 <?php
+Route::group(['middleware' => ['web']], function () {
 Route::post('/logging', '\App\Http\Controllers\Auth\LoginController@login');
 Route::get('/major/subject/{id}', '\App\Modules\Major\Controllers\SubjectController@index');
+});
