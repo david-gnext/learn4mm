a5ce616cb439b98fa97735dbf6b09d281838a82b
david.jaw.hpan@myanmar.gnext.asia <david.jaw.hpan@myanmar.gnext.asia>
2017-12-04 11:25:18 +0630

    subject

--- app/Modules/Major/Controllers/SubjectController.php
+++ app/Modules/Major/Controllers/SubjectController.php
@@ -19,11 +19,10 @@ class SubjectController extends Controller
     public function __construct() {
         $this->middleware("auth")->except("logout");
     }
-    
+
     public function index($id) {
-        $subjects = DB::select('select s.id as sid,s.name as sname,s.description,s.isRead as rd,m.color as class from subject as s left join major as m on m.id = s.majorid where s.majorid = ? and s.deleted_flag = 0',[$id]);
+        $subjects = DB::select('select s.id as sid,s.name as sname,s.description,s.isread as rd,m.color as class from subject as s left join major as m on m.id = s.majorid where s.majorid = ? and s.deleted_flag = 0',[$id]);
         return view('Major::subject/index', ['subjects' => $subjects]);
-        
+
     }
 }
-
