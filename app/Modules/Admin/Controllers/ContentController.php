bc8727de4bf3607fb333ee5073b1544807f32c44
david.jaw.hpan@myanmar.gnext.asia <david.jaw.hpan@myanmar.gnext.asia>
2017-12-04 11:13:36 +0630

    d

--- app/Modules/Admin/Controllers/ContentController.php
+++ app/Modules/Admin/Controllers/ContentController.php
@@ -52,8 +52,9 @@ class ContentController extends BaseController
     public function save(Request $request,$id) {
         if($id == "new") {
             DB::table("content")->insert(
-                    ['subjectid'=>$request->subjectId,'content_main'=>$request->name,'content_mm'=>$request->mm,'q1'=>$request->q1,'q2'=>$request->q2,'q3'=>$request->q3,'ans'=>$request->ans,'hint'=>$request->hint,'isFill'=>$request->isRead,'audio'=>$request->audio,'img'=>$request->img,'created_time'=>date("Y-m-d h:i:s"),
-                    ,'deleted_flag'=>0]
+                    ['subjectid'=>$request->subjectId,'content_main'=>$request->name,'content_mm'=>$request->mm,'q1'=>$request->q1,'q2'=>$request->q2,'q3'=>$request->q3,'ans'=>$request->ans,'hint'=>$request->hint,'isFill'=>$request->isRead,'audio'=>$request->audio,'img'=>$request->img,
+                     'created_time'=> date('Y-m-d h:i:s'),
+                     'deleted_flag'=>0]
                     );
             echo json_encode(array("code"=>200,"msg"=>"Created Successfully"));
         } else {
