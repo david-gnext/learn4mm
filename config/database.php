467fb04a1953d667df6c30c4c63e546d1adab01d
david-gnext <david.gnext@gmail.com>
2017-12-19 21:59:50 +0630

    test api

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'pgsql'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -43,7 +43,7 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'learn4mm'),
+            'database' => env('DB_DATABASE', 'test'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
+use App\Article;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +13,49 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+         $this->call(UsersTableSeeder::class);
+         $this->call(ArticlesTableSeeder::class);
     }
+}
+
+class ArticlesTableSeeder extends Seeder
+{
+	public function run()
+	{
+		Article::truncate();
+		$faker = \Faker\Factory::create();
+
+		for($i = 0; $i < 50; $i++) {
+			Article::create([
+				'title' => $faker->sentence,
+				'body' => $faker->paragraph,
+				]);
+		}
+	}
+}
+
+class UsersTableSeeder extends Seeder 
+{
+	public function run()
+	{
+		User::truncate();
+
+		$faker = \Faker\Factory::create();
+		$password = Hash::make('toptal');
+		User::create([
+			'name' => 'admin',
+			'email' => 'admin@test.com',
+			'password' => $password,
+			'password_confirmation' => $password,
+			]);
+
+		 for ($i = 0; $i < 10; $i++) {
+            User::create([
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'password' => $password,                
+				'password_confirmation' => $password,
+            ]);
+        }
+	}
 }
